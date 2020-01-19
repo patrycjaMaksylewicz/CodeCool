27b8444400c0d6fe832a15361cef625f8a5ad8cb
Patrycja Maksylewicz <patiamail@gmail.com>
2020-01-19 15:06:03 +0100

    Initial Commit

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+i = 1
+next_number = 1
+previous_number = 0
+fib = 0
+fib_length = int(input("Please write how many numbers count? "))
+
+print('Fibonacci sequance:')
+while i <= fib_length:
+    result = str(fib)
+    print(str(i) + '. ' + result.rjust(30,' '))
+    fib = next_number  + previous_number
+    next_number = previous_number
+    previous_number = fib
+    i = i + 1
+
